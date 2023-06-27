45b46f5b70c4cb22316ca2d285bc3292865eb680
Yunsun Jung <yunsun992510@gmail.com>
2023-06-27 19:34:13 +0900

    Feat: 2일차 문제 코드 구현(+ 시간초과 해결)

--- day2.py
+++ day2.py
@@ -9,13 +9,15 @@ def solution(r1, r2):
         # 반지름 제곱해서 가로 제곱을 빼고 루트 씌워서 세로 확인
         # 이 두개 사이에 정수가 있는지.. 어떻게 확인하지
         big_y = floor((r2**2 - i**2)**(1/2))
-        if i > r1:
+        if i >= r1:
             small_y = 0
+            answer += floor(big_y - small_y) + 1
         else:
             small_y = ceil((r1**2 - i**2)**(1/2))
-        
-        for i in range(small_y, big_y + 1):
-            answer += 1
+            if type(big_y) is int or type(small_y) is int:
+                answer += floor(big_y - small_y) + 1
+            else:
+                answer += floor(big_y - small_y)
     
     # 제 1사분면 위의 점의 갯수에 4 곱하기
     answer *= 4
