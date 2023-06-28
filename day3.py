52a82ee235fec82b000f08ec86f606d33fe8d3fd
Yunsun Jung <yunsun992510@gmail.com>
2023-06-28 23:58:08 +0900

    Feat: 3일차 문제 코드 구현

--- day3.py
+++ day3.py
@@ -1,23 +1,20 @@
 def solution(sequence, k):
     answer = []
-    # 뒤에서 부터 빼는데, 만약 빼다가 마이너스가 되면 맨 뒤에꺼는 다시 더해준다.
-    temp = k
-    seq_len = len(sequence)
-    j = seq_len - 1
-    temp_seq_len = seq_len
-    
+    l = 0
+    s = -1
+    temp = 0
     while True:
-        if temp == 0:
-            while sequence[j] == sequence[temp_seq_len - 1]:
-                answer.extend([temp_seq_len - 1, j - 1])
+        if temp < k:
+            s += 1
+            if s >= len(sequence):
                 break
-            answer.extend([temp_seq_len, j])
-            break
-        elif temp < 0:
-            temp += sequence[j]
-            j -= 1
-        temp -= sequence[temp_seq_len - 1]
-        temp_seq_len -= 1
-
-    
-    return answer
+            temp += sequence[s]
+        else:
+            temp -= sequence[l]
+            if l >= len(sequence):
+                break
+            l += 1
+        if temp == k:
+            answer.append([l, s])
+    answer.sort(key=lambda x: (x[1]-x[0], x[0]))
+    return answer[0]
